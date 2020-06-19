2280a9697bbd31f6ed879b7c8a46de7c89d670fb
Django Freeman <django@okgs.me>
2020-06-19 12:01:11 +0800

    1

--- class8.c
+++ class8.c
@@ -3,6 +3,7 @@
 int main()
 {
 	/*指针*/
+	/*
 	int num = 8;
 	int * ptr_num = &num;
 	//头疼点：指针的指针，定义式需要两个*号
@@ -19,5 +20,23 @@ int main()
 	*ptr_num = 9999;
 	printf("*ptr_num对应的值为：%d\n", num);
 	return 0;
+	*/
+
+
+	int num1 = 1024;
+	int num2 = 2048;
+	int* ptr1;
+	int* ptr2;
+	ptr1 = &num1;
+	ptr2 = &num2;
+	printf("num1的值是%d\tnum1的地址是：%p\n", num1, ptr1);
+	printf("num2的值是%d\tnum2的地址是：%p\n", num2, ptr2);
+	//将变量1的值，赋给变量2
+	//num2 = num1; //等价于 *ptr2 = *ptr1;
+	//直接操作指针
+	ptr1 = ptr2;
+	printf("重新赋值后：\n");
+	printf("num1的值是%d\tnum1的地址是：%p\n", num1, ptr1);
+	printf("num2的值是%d\tnum2的地址是：%p\n", num2, ptr2);
 
 }
